0d94ea155a90fc96b708a988c4d51f012a6ebe89
aneeshbot <76258732+aneeshbot@users.noreply.github.com>
2022-04-07 17:01:11 -0400

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,10 @@
+function addUser(){
+    player_1 = document.getElementById("player_1").value;
+    player_2 = document.getElementById("player_2").value;
+    localStorage.setItem("player_1", player_1);
+    localStorage.setItem("player_2", player_2);
+    
+    window.location = "quiz_game_page.html";
+    
+    
+    }
